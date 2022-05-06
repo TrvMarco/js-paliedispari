0302e5247959f1d7e0b74ffd73e0a5fe63a27b28
TrvMarco <trevisanello.marco@gmail.com>
2022-05-06 18:09:20 +0200

    abbozzato codice

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,3 +1,24 @@
 // 1. Chiedere all’utente di inserire una parola
-const userWord = prompt("Inserisci una parola")
+const userWord = prompt("Inserisci una parola");
 // 2. Creare una funzione per capire se la parola inserita è palindroma
+
+// function palindromeword (str){
+//     let splitString = str.split("");
+//     let reverseStr = splitString.reverse().joinArray('');
+//     console.log(reverseStr);
+// }
+
+// palindromeword(userWord)
+// console.log(palindromeword)
+
+let parolaInvertita = "";
+
+function palidnrome (wordPalindrome){
+    for( i = userWord.length - 1; i >= 0; i--){
+        parolaInvertita += userWord[i];
+    }
+    
+    if(userWord == parolaInvertita){
+        console.log("Parola palindroma")
+    }return
+}
